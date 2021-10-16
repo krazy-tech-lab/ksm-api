e49f126eae6ec06030ec487a9c45ce8a5fb67df0
KRAZy TECH Lab <krazy.tech.lab@gmail.com>
2021-10-16 11:19:53 +0900

    first

---
+++ src/main/java/com/krazy/ksmapi/KsmApiApplication.java
@@ -0,0 +1,13 @@
+package com.krazy.ksmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krazy/ksmapi/KsmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krazy.ksmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
